860392e3e569f4e95de3ad3f57a3395cc3efec65
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-06-17 13:20:49 +0100

    Fix column names in backup.

--- backup/moodle2/backup_qtype_highlightwords_plugin.class.php
+++ backup/moodle2/backup_qtype_highlightwords_plugin.class.php
@@ -48,7 +48,7 @@ class backup_qtype_highlightwords_plugin extends backup_qtype_plugin {
 
         // Now create the qtype own structures.
         $highlightwords = new backup_nested_element('highlightwords', array('id'), array(
-            'delimitchars', 'correctfedback', 'correctfeddbackformat', 'partiallycorrectfeedback',
+            'delimitchars', 'correctfeedback', 'correctfeedbackformat', 'partiallycorrectfeedback',
             'partiallycorrectfeedbackformat',
             'incorrectfeedback', 'incorrectfeedbackformat'));
 
